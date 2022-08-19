565187b0b6c335a7d04e50dfd614a3aeb99c7cf3
sidhu <sidhusoman101@gmail.com>
2022-08-19 20:53:20 +0530

    Date and Time

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace DateTimeClass{
+  class Time{
+    static void Main(string[] args){
+      DateTime Hello = DateTime.Now;
+      Console.WriteLine(Hello.ToString("dddd,MMMM"));
+    }
+  }
+}
